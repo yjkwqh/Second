5b6f66669afce12bd45f47ddb45a6b5539073de6
wqh <1179976990@qq.com>
2019-03-15 22:44:08 +0800

    my second commit

---
+++ app/src/main/java/com/example/second_test/Second_test.java
@@ -0,0 +1,13 @@
+package com.example.second_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Second_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Table_layout);
+    }
+}
